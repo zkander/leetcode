958137cb50aa2c97f0912fe24d3651e9e8506775
Aly Soliman <alysoliman05@gmail.com>
2023-10-13 21:17:55 +0300

    537 edit

--- 537.complex-number-multiplication.py
+++ 537.complex-number-multiplication.py
@@ -7,55 +7,59 @@
 # @lc code=start
 class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
-        sign = False
-        real = ""
-        nums = []
-        for s in num1:
-            if ((s == "-" or s == "+") and (real != "-" and real != "+" and real != "")):
-                sign == True
-                nums.append(real)
-                real = s
+        # sign = False
+        # real = ""
+        # nums = []
+        # for s in num1:
+        #     if ((s == "-" or s == "+") and (real != "-" and real != "+" and real != "")):
+        #         sign == True
+        #         nums.append(real)
+        #         real = s
 
-            if ((s == "-" or s == "+") and (real == "-" or real == "+" or real == "")):
-                real = s
+        #     if ((s == "-" or s == "+") and (real == "-" or real == "+" or real == "")):
+        #         real = s
 
-            elif (s == "i"):
-                nums.append(real)
+        #     elif (s == "i"):
+        #         nums.append(real)
 
-            elif (s.isnumeric()):
-                if (sign):
-                    sign = False
-                real = real + s
-        sign = False
-        real = ""
-        for s in num2:
-            if (((s == "-" or s == "+") and (real != "-" and real != "+" and real != ""))):
-                sign == True
-                nums.append(real)
-                real = s
+        #     elif (s.isnumeric()):
+        #         if (sign):
+        #             sign = False
+        #         real = real + s
+        # sign = False
+        # real = ""
+        # for s in num2:
+        #     if (((s == "-" or s == "+") and (real != "-" and real != "+" and real != ""))):
+        #         sign == True
+        #         nums.append(real)
+        #         real = s
 
-            if ((s == "-" or s == "+") and (real == "-" or real == "+" or real == "")):
-                real = s
+        #     if ((s == "-" or s == "+") and (real == "-" or real == "+" or real == "")):
+        #         real = s
 
-            elif (s == "i"):
-                nums.append(real)
+        #     elif (s == "i"):
+        #         nums.append(real)
 
-            elif (s.isnumeric()):
-                if (sign):
-                    sign = False
-                real = real + s
-       
+        #     elif (s.isnumeric()):
+        #         if (sign):
+        #             sign = False
+        #         real = real + s
 
-        arr = []
+        # arr.append(int(nums[0]) * int(nums[2]))
+        # arr.append(int(nums[0]) * int(nums[3]))
+        # arr.append(int(nums[1]) * int(nums[2]))
+        # arr.append((int(nums[1]) * int(nums[3])) * -1)
 
-        arr.append(int(nums[0]) * int(nums[2]))
-        arr.append(int(nums[0]) * int(nums[3]))
-        arr.append(int(nums[1]) * int(nums[2]))
-        arr.append((int(nums[1]) * int(nums[3])) * -1)
+        # i = arr[1] + arr[2]
+        # r = arr[0] + arr[3]
 
-        i = arr[1] + arr[2]
-        r = arr[0] + arr[3]
-
+        # return str(r) + "+" + str(i) + "i"
+        num1 = num1.replace("i", "", 1)
+        num2 = num2.replace("i", "", 1)
+        a, b = (num1.split("+"))
+        c, d = (num2.split("+"))
+        r = (int(a) * int(c)) + ((int(b) * int(d)) * -1)
+        i = ((int(a) * int(d)) + (int(b) * int(c)))
         return str(r) + "+" + str(i) + "i"
 
 
