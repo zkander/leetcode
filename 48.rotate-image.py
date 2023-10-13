3104765fc2f5c73138541294c5439f8260cc0528
Aly Soliman <alysoliman05@gmail.com>
2023-10-13 17:57:38 +0300

    48

---
+++ 48.rotate-image.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=48 lang=python3
+#
+# [48] Rotate Image
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()
+
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+# @lc code=end
